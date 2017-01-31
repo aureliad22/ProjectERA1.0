a461c06f7be272a30f03738e370f702388ffe003
Aurelia Delaune <aurelia.delaune@gmail.com>
2017-01-31 15:26:23 +0100

    Resolving conflicts bis

--- src/main/java/com/projectERA/controller/view/TeacherViewController.java
+++ src/main/java/com/projectERA/controller/view/TeacherViewController.java
@@ -1,41 +1,3 @@
-<<<<<<< HEAD
-//package com.projectERA.controller.view;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.ModelAttribute;
-//import org.springframework.web.bind.annotation.PostMapping;
-//
-//import com.projectERA.dao.interfaces.ITeacherDao;
-//import com.projectERA.model.DumpFields;
-//import com.projectERA.model.Teacher;
-//
-//
-//@Controller
-//public class TeacherViewController {
-//	@Autowired
-//	private ITeacherDao teacherDao;
-//	
-//	@GetMapping("/teachers/create")
-//	public String userCreate(Model model) {
-//		model.addAttribute("attributs", DumpFields.<Teacher>inspect(Teacher.class));
-//		return "teacher/teacherCreate";
-//	}
-//
-//	@PostMapping("/teachers/create")
-//	public String itemTestRetreiver(@ModelAttribute Teacher teacher){
-//		/*Teacher teacher = new Teacher(firstname, lastname, login, password);*/
-//		System.out.println(teacher);
-//		teacherDao.create(teacher);
-//
-//		String redirectUrl = "/";
-//	    return "redirect:" + redirectUrl;
-//	}
-//}
-//
-=======
 package com.projectERA.controller.view;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -74,5 +36,3 @@ public class TeacherViewController {
 	    return "redirect:" + redirectUrl;
 	}
 }
-
->>>>>>> 922e8a586ba04cd774411effa1b509846e8e482a
